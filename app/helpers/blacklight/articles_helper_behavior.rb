b1689135e332d126ab6bf44068eb399390447238
Eric Frierson <efrierson@ebscohost.com>
2013-10-28 12:34:43 -0500

    Eliminating extra call to SEARCH method

--- app/helpers/blacklight/articles_helper_behavior.rb
+++ app/helpers/blacklight/articles_helper_behavior.rb
@@ -309,6 +309,7 @@ module Blacklight::ArticlesHelperBehavior
     if Time.now.getutc.to_i < timestamp.to_i
       return true
     else
+      session[:debugNotes] << "<p>Looks like the auth token is out of date.. It expired at " << Time.at(timestamp.to_i).to_s << "</p>"
       return false
     end
   end
@@ -1324,7 +1325,7 @@ module Blacklight::ArticlesHelperBehavior
   end
   
   def debugNotes
-    return session[:debugNotes]
+    return session[:debugNotes] << "<h4>API Calls</h4>" << @connection.debug_notes
   end
 
 end

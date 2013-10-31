2c80cdbcddb9549645c062da6a5956bd84023c39
Eric Frierson <efrierson@ebscohost.com>
2013-10-31 09:35:18 -0500

    Prep for public load to GitHub

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller 
-  before_filter :authenticate_user!
-
    include Blacklight::Controller
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
   # these methods in order to perform user specific actions. 
